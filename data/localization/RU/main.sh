5f89537ab6a735157c556465361cb1f5631b3edb
intervisionlord <ads-net@mail.ru>
2019-10-23 22:54:58 +0300

    Локализация
    + Правка и дополнение локализации

--- data/localization/RU/main.sh
+++ data/localization/RU/main.sh
@@ -10,7 +10,7 @@ LANSTR_JOB_COMPLETED='Работа завершена.'
 LANSTR_TORRENTS_FOUND='Найдено торрент файлов'
 
 #ReTrackers total
-LANSTR_RETRACKERS_TOTAL="Общее количество ретрекеров"
+LANSTR_RETRACKERS_TOTAL='Общее количество ретрекеров'
 
 # Using Recirsive mode for folder
 LANSTR_USING_RECURSIVE='Используется рекурсивный режим для директории'
